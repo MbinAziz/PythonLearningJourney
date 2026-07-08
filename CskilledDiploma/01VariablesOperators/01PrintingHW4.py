e7e3eb51bfb0f68ea885269114736f419e4ad262
Mahmoud Aziz <mbin.aziz@outlook.com>
2026-07-08 11:44:57 +0300

    Add pyramid pattern printing code
    Implemented code to print a pyramid pattern.

---
+++ CskilledDiploma/01VariablesOperators/01PrintingHW4.py
@@ -0,0 +1,11 @@
+''' this my 4homework , Write code to print the following '''
+'''
+         *
+       * * *
+     * * * * *
+   * * * * * * *
+ * * * * * * * * *
+'''
+#My soluation
+
+print('         *\n       * * *\n     * * * * *\n   * * * * * * *\n * * * * * * * * *')
