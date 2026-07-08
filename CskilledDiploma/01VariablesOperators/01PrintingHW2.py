f8c8c4d672cc91e0464c79aba8bd26b8fd852c34
Mahmoud Aziz <mbin.aziz@outlook.com>
2026-07-08 11:44:06 +0300

    Add 01PrintingHW2.py for homework assignment
    Implemented a Python program to match specified output.

---
+++ CskilledDiploma/01VariablesOperators/01PrintingHW2.py
@@ -0,0 +1,14 @@
+''' this my 2homework '''
+''' Write a program that its output matches exactly this output
+You should write EXACTLY 4 commands
+Your output must match EXACTLY the below output
+
+Print("Me")
+Print('Me')
+Print('Me")
+You will learn
+ a LOT
+'''
+#My soluation
+
+print("Print(\"Me\")\nPrint(\'Me\')\nPrint(\'Me\")\nYou will learn\n a LOT  ")
