68cc2ab2a3860c45d0ed483b82ad0849065e4341
Mahmoud Aziz <mbin.aziz@outlook.com>
2026-07-08 11:46:14 +0300

    Fix syntax errors in 01PrintingHW6.py
    Corrected print statements and fixed syntax errors.

---
+++ CskilledDiploma/01VariablesOperators/01PrintingHW6.py
@@ -0,0 +1,16 @@
+''' this my 6homework , Correct me
+print(Hi)
+print()
+print"I am mostafa")
+print(3 * 6)
+print("Wonoderful " day)
+print("Wondorful " day")
+'''
+
+#MySoluation
+print('Hi')
+print('')
+print("I\'am Mostafa")
+print(3 * 6)
+print("Wonderful day")
+print("Wonderful \" day")
