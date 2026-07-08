e5b1e4a810e2b0d029f03bd661262258a2a72c9c
Mahmoud Aziz <mbin.aziz@outlook.com>
2026-07-08 11:43:12 +0300

    Create 01PrintingHW1.py

---
+++ CskilledDiploma/01VariablesOperators/01PrintingHW1.py
@@ -0,0 +1,16 @@
+''' this my 1homework, guess the run-time output TRACING '''
+print('Practice', " Makes Perfect.")
+print("Children must be taught\n  how to think\n  NOT\n  what to think")
+print("\"\"")
+print('2 * 3 * 4 * 5 / 10 = ')
+print(2 * 3 * 4 * 5 / 10)
+
+#My soluation
+Practice  Makes Perfect.
+Children must be taught
+  how to think
+  NOT
+  what to think
+""
+2 * 3 * 4 * 5 / 10 =
+12.0
