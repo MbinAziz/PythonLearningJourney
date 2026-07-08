2a4fb030454c7fd71fd6e9d65be9ef5ebcb30b0c
Mahmoud Aziz <mbin.aziz@outlook.com>
2026-07-08 11:45:16 +0300

    Add diamond pattern printing in 01PrintingHW5.py
    Implemented a program to print a diamond pattern using asterisks.

---
+++ CskilledDiploma/01VariablesOperators/01PrintingHW5.py
@@ -0,0 +1,24 @@
+''' this my 5homework , Write code to print the following '''
+'''
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *     
+        *       
+'''
+#My soluation
+
+
+print("        *")
+print("      * * *")
+print("    * * * * *")
+print("  * * * * * * *")
+print("* * * * * * * * *")
+print("  * * * * * * *")
+print("    * * * * *")
+print("      * * *")
+print("        *")01PrintingHW1.py
