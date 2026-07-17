7fdb0130ebca19b8514235469fe6b3b43c8740d7
Mahmoud Aziz <mbin.aziz@outlook.com>
2026-07-17 05:20:05 +0300

    Add files via upload

---
+++ CskilledDiploma/01VariablesOperators/2data_types_vars_medium_hw1.py
@@ -0,0 +1,29 @@
+#Guess Output
+num1 = 1
+num2 = 2
+
+num3 = num1 + num2 # 3
+num1 = num2 # 2
+num2 = num3 #3
+
+num3 = num1 + num2 #5
+num1 = num2 #3
+num2 = num3 #5
+
+num3 = num1 + num2 #8
+num1 = num2 #5
+num2 = num3 #8
+
+num3 = num1 + num2 #13
+num1 = num2 #8
+num2 = num3 #13
+
+num3 = num1 + num2 #21
+num1 = num2 #13
+num2 = num3 #21
+
+num3 = num1 + num2 #34
+num1 = num2 #13
+num2 = num3 #34
+
+print(num3)
---
+++ CskilledDiploma/01VariablesOperators/2data_types_vars_medium_hw2.py
@@ -0,0 +1,8 @@
+num1, num2 = map(int, input().split())
+
+temp = num1 # TODO Line #1
+num1 = num2 # TODO Line #2
+num2 = temp # TODO Line #3
+
+
+print(num1, num2)
